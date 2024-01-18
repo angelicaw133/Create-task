6e2bb0f73a42ec30b01ec933e375ee75c3640ebe
angelicaw133 <angelicawo426@gmail.com>
2024-01-18 10:15:44 -0500

    Update create.js

--- create.js
+++ create.js
@@ -1,61 +1,62 @@
-function start(){
-   const URL = 'https://api.quotable.io/random'
-    getData(URL)
+function start() {
+  const URL = "https://api.quotable.io/random";
+  getData(URL);
 }
-start()
+start();
 
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        if (response.status != 200) {
-            throw new Error(response.statusText);
-        }
-        const data = await response.json();
-        console.log(data)
-        document.querySelector("h2").textContent = data.content;
-        let author = data.author;
-        let quote = data.content;
-        check(author, quote);
-    } catch (error) {
-        console.log(error, "please try again later")
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status != 200) {
+      throw new Error(response.statusText);
     }
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("h2").textContent = data.content;
+    let author = data.author;
+    let quote = data.content;
+    let guess = document.querySelector(".guess_author").value;
+    let btn = document.querySelector(".button");
+
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      check(author, quote, guess);
+
+      console.log(guess);
+      // check(guess, author)
+    });
+  } catch (error) {
+    console.log(error, "please try again later");
+  }
 }
 
-function insert_wrong(quote, author) {
-    document.querySelector(".arr_container").insertAdjacentHTML("afterbegin",
+function insert_wrong(quote, author, guess) {
+  document.querySelector(".arr_container").insertAdjacentHTML(
+    "afterbegin",
     ` <div class="wrong_ans">
-    <h2>${quote}</h2>
-    <h3>${author}</h3>
+    <h2>Quote: ${quote}</h2>
+    <h3>Your Guess: ${guess}</h3>
     <h3>you are wrong!</h3>
-    <h3>right answer</h3>
-</div>`)
+    <h3>Right Answer: ${author}</h3>
+</div>`
+  );
 }
 
-function insert_right(quote, author){
-    document.querySelector(".arr_container").insertAdjacentHTML("afterbegin",
+function insert_right(quote, guess) {
+  document.querySelector(".arr_container").insertAdjacentHTML(
+    "afterbegin",
     `   <div class="right_ans">
-    <h2>${quote}</h2>
-    <h3>${author}</h3>
-    <h3>you are rihgt!</h3>
-</div>`)
+    <h2>Quote: ${quote}</h2>
+    <h3>Your Guess: ${guess}</h3>
+    <h3>you are Right!</h3>
+</div>`
+  );
 }
 
-
-
-function check(author, quote) { 
-    let btn = document.querySelector(".button")
-    
-    btn.addEventListener("click", function(event) {
-        event.preventDefault();
-
-        let guess = document.querySelector(".guess_author").value
-        console.log(guess)
-        console.log(data.author)
-        // check(guess, author)
-    })
-    if (guess == author) {
-        return insert_right(quote, author)
-    } else {
-        return insert_wrong(quote, author)
-    }
+function check(author, quote, guess) {
+  if (guess == author) {
+    return insert_right(quote, guess);
+  } else {
+    return insert_wrong(quote, author, guess);
+  }
 }
